01d898dc947ba328e0b8cc5439f4e4ffea9e5418
espahr <spahre11@gmail.com>
2019-03-06 13:50:00 -0500

    add config yaml

--- jobs/seed_all.groovy
+++ jobs/seed_all.groovy
@@ -1,23 +1,24 @@
-job('GeneratedJobs/New_Job_with_Git_Groovy-1') {
-	scm {
-    	git('git://github.com/quidryan/aws-sdk-test.git')
-    }
-    triggers {
-    	scm('H/15 * * * *')
-    }
-    steps {
-    	maven('-e clean test')
-    }
+#!groovy
+
+def jobConfig = [:]
+def jobConfigText = libraryResource("ftb_job_names.yaml")
+
+jobConfig = readYaml text: "${jobConfigText}"
+
+if( !jobConfig?.ftp_app_names ){
+  error "not able to read yaml file"
 }
 
-job('GeneratedJobs/New_Job_with_Git_Groovy-2') {
-	scm {
-    	git('git://github.com/quidryan/aws-sdk-test.git')
-    }
-    triggers {
-    	scm('H/15 * * * *')
-    }
-    steps {
-    	maven('-e clean test')
-    }
+for( jobName in jobConfig.ftp_app_names ) {
+  job('GeneratedJobs/Job-DSL-Test-${jobName}') {
+  	scm {
+      	git('git://github.com/quidryan/aws-sdk-test.git')
+      }
+      triggers {
+      	scm('H/15 * * * *')
+      }
+      steps {
+      	maven('-e clean test')
+      }
+  }
 }
