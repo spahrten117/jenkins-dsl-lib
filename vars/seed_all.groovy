db25fdb88b7b671b623c1262a9416dd299aaad8b
espahr <spahre11@gmail.com>
2019-03-07 15:15:21 -0500

    add pipeline file

---
+++ vars/seed_all.groovy
@@ -0,0 +1,24 @@
+#!groovy
+
+def jobConfig = [:]
+def jobConfigText = libraryResource("ftb_job_names.yaml")
+
+jobConfig = readYaml text: "${jobConfigText}"
+
+if( !jobConfig?.ftp_app_names ){
+  error "not able to read yaml file"
+}
+
+for( jobName in jobConfig.ftp_app_names ) {
+  job('GeneratedJobs/Job-DSL-Test-${jobName}') {
+  	scm {
+      	git('git://github.com/quidryan/aws-sdk-test.git')
+      }
+      triggers {
+      	scm('H/15 * * * *')
+      }
+      steps {
+      	maven('-e clean test')
+      }
+  }
+}
