35ce88c1524c70e4778d6d39eb930a0c3868f740
espahr <spahre11@gmail.com>
2019-03-07 15:57:00 -0500

    make seed_all.groovy just a dsl job script

--- jobs/seed_all.groovy
+++ jobs/seed_all.groovy
@@ -1,15 +1,5 @@
 #!groovy
 
-def jobConfig = [:]
-def jobConfigText = libraryResource("ftb_job_names.yaml")
-
-jobConfig = readYaml text: "${jobConfigText}"
-
-if( !jobConfig?.ftp_app_names ){
-  error "not able to read yaml file"
-}
-
-for( jobName in jobConfig.ftp_app_names ) {
   job('GeneratedJobs/Job-DSL-Test-${jobName}') {
   	scm {
       	git('git://github.com/quidryan/aws-sdk-test.git')
@@ -21,4 +11,3 @@ for( jobName in jobConfig.ftp_app_names ) {
       	maven('-e clean test')
       }
   }
-}
