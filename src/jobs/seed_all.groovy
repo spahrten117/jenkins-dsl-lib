7e8e967a478ebbc576e11f242dbb7346cda40047
espahr <spahre11@gmail.com>
2019-03-08 13:50:33 -0500

    usd jobDsl pipeline step

--- src/jobs/seed_all.groovy
+++ src/jobs/seed_all.groovy
@@ -1,15 +1,15 @@
 #!groovy
 
-def jobConfig = [:]
-def jobConfigText = libraryResource("ftb_job_names.yaml")
-
-jobConfig = readYaml text: "${jobConfigText}"
-
-if( !jobConfig?.ftp_app_names ){
-  error "not able to read yaml file"
-}
-
-for( jobName in jobConfig.ftp_app_names ) {
+// def jobConfig = [:]
+// def jobConfigText = libraryResource("ftb_job_names.yaml")
+//
+// jobConfig = readYaml text: "${jobConfigText}"
+//
+// if( !jobConfig?.ftp_app_names ){
+//   error "not able to read yaml file"
+// }
+//
+// for( jobName in jobConfig.ftp_app_names ) {
   job('GeneratedJobs/Job-DSL-Test-${jobName}') {
       scm {
         git('git://github.com/quidryan/aws-sdk-test.git')
@@ -21,4 +21,4 @@ for( jobName in jobConfig.ftp_app_names ) {
         maven('-e clean test')
       }
   }
-}
+// }
