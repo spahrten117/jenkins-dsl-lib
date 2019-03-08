3c9bd79265608fb7349620e53f2f4c9a7d4af3e4
espahr <spahre11@gmail.com>
2019-03-08 15:19:19 -0500

    usd jobDsl pipeline step

--- src/jobs/seed_all.groovy
+++ src/jobs/seed_all.groovy
@@ -2,15 +2,29 @@
 //def ftp_app_names = [ "app1", "app2", "app3", "app4", "app5", "app6", "app7", "app8", "app9", "app10" ]
 
 for( jobName in jobConfig.ftp_app_names ) {
-  job("GeneratedJobs/Job-DSL-Test-${jobName}") {
-      scm {
-        git('git://github.com/quidryan/aws-sdk-test.git')
-      }
-      triggers {
-        scm('H/15 * * * *')
-      }
-      steps {
-        maven('-e clean test')
-      }
+  if(jobName == "app1"){
+    job("GeneratedJobs/Job-DSL-Test-${jobName}") {
+        scm {
+          git('git://github.com/quidryan/aws-sdk-test.git')
+        }
+        triggers {
+          githubPush()
+        }
+        steps {
+          maven('clean test')
+        }
+    }
+  } else {
+    job("GeneratedJobs/Job-DSL-Test-${jobName}") {
+        scm {
+          git('git://github.com/quidryan/aws-sdk-test.git')
+        }
+        triggers {
+          scm('H/15 * * * *')
+        }
+        steps {
+          maven('-e clean test')
+        }
+    }
   }
 }
