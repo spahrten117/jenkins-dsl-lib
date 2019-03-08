5742a7cb09e730fa60075a240bf5cc104b62c5db
espahr <spahre11@gmail.com>
2019-03-08 14:24:43 -0500

    usd jobDsl pipeline step

--- src/jobs/seed_all.groovy
+++ src/jobs/seed_all.groovy
@@ -1,17 +1,17 @@
 #!groovy
 
-// def jobConfig = [:]
-// def jobConfigText = libraryResource("ftb_job_names.yaml")
+def jobConfig = [:]
+def jobConfigText = libraryResource("reasources/ftb_job_names.yaml")
 
-// jobConfig = readYaml text: "${jobConfigText}"
+jobConfig = readYaml text: "${jobConfigText}"
 
-// if( !jobConfig?.ftp_app_names ){
-//   error "not able to read yaml file"
-// }
+if( !jobConfig?.ftp_app_names ){
+  error "not able to read yaml file"
+}
 
-def ftp_app_names = [ "app1", "app2", "app3", "app4", "app5", "app6", "app7", "app8", "app9", "app10" ]
+//def ftp_app_names = [ "app1", "app2", "app3", "app4", "app5", "app6", "app7", "app8", "app9", "app10" ]
 
-for( jobName in ftp_app_names ) {
+for( jobName in jobConfig.ftp_app_names ) {
   job("GeneratedJobs/Job-DSL-Test-${jobName}") {
       scm {
         git('git://github.com/quidryan/aws-sdk-test.git')
