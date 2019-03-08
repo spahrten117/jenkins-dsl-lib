375adc386852abe02fb321a14f38f77a896d1be3
espahr <spahre11@gmail.com>
2019-03-08 08:44:51 -0500

    call seed_all from pipeline

--- vars/seed_all.groovy
+++ vars/seed_all.groovy
@@ -1,15 +1,15 @@
 #!groovy
 def call (){
-  def jobConfig = [:]
-  def jobConfigText = libraryResource("ftb_job_names.yaml")
-
-  jobConfig = readYaml text: "${jobConfigText}"
-
-  if( !jobConfig?.ftp_app_names ){
-    error "not able to read yaml file"
-  }
-
-  for( jobName in jobConfig.ftp_app_names ) {
+  // def jobConfig = [:]
+  // def jobConfigText = libraryResource("ftb_job_names.yaml")
+  //
+  // jobConfig = readYaml text: "${jobConfigText}"
+  //
+  // if( !jobConfig?.ftp_app_names ){
+  //   error "not able to read yaml file"
+  // }
+  //
+  // for( jobName in jobConfig.ftp_app_names ) {
     job('GeneratedJobs/Job-DSL-Test-${jobName}') {
     	scm {
         	git('git://github.com/quidryan/aws-sdk-test.git')
@@ -21,5 +21,5 @@ def call (){
         	maven('-e clean test')
         }
     }
-  }
+  // }
 }
